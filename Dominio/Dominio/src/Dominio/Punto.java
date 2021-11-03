178e32e59ce443c9ebd5424d3142f35483446ed8
Carlos <Carlos@DESKTOP-EDKMT4J>
2021-11-03 11:51:11 -0700

    Dominio

---
+++ Dominio/Dominio/src/Dominio/Punto.java
@@ -0,0 +1,14 @@
+/**
+ * Punto.java
+ */
+package Dominio;
+
+/**
+ *
+ * @author Equipo 3
+ */
+public class Punto extends Forma{
+    public Punto(int width, int height, int x, int y) {
+        super(width, height, x, y);
+    }  
+}
